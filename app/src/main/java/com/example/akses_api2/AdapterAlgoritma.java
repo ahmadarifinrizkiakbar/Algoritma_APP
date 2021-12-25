ba77dec20c5c5e4a09d994660d4ab558d04a9143
ASUS <ahmadarifinrizkiakbar@gmail.com>
2021-12-25 15:09:21 +0800

    second commit

---
+++ app/src/main/java/com/example/akses_api2/AdapterAlgoritma.java
@@ -0,0 +1,47 @@
+package com.example.akses_api2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.akses_api2.Model.DataAlgoritma;
+
+import java.util.ArrayList;
+
+public class AdapterAlgoritma extends ArrayAdapter{
+    ArrayList<DataAlgoritma> dataAlgoritmas= new ArrayList();
+    LayoutInflater layoutInflater;
+    Context context;
+
+    public AdapterAlgoritma(Context context, ArrayList<DataAlgoritma> dataAlgoritmas) {
+        super(context, R.layout.list_algoritma, dataAlgoritmas);
+        this.dataAlgoritmas = dataAlgoritmas;
+        layoutInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = layoutInflater.inflate(R.layout.list_algoritma, parent, false);
+        DataAlgoritma dataAlgoritma = this.dataAlgoritmas.get(position);
+
+        TextView titleListview = convertView.findViewById(R.id.titleListview);
+        ImageView imageView = convertView.findViewById(R.id.imgListview);
+
+        Glide
+                .with(context)
+                .load(dataAlgoritma.getGambar())
+                .placeholder(R.drawable.ic_refres_foreground)
+                .into(imageView);
+
+        titleListview.setText(dataAlgoritma.getNameAlgoritma());
+
+
+        return convertView;
+    }
+}
